e5955b8149fe9d1f932773066efa59d00a2f835c
zyn <zyn@qq.com>
2022-03-12 16:06:50 +0800

    firsttttt

---
+++ src/main/java/com/hpu/test.java
@@ -0,0 +1,12 @@
+package com.hpu;
+
+/**
+ * @author zyn
+ * @version 1.0
+ * @date 2022/3/12 15:59
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
